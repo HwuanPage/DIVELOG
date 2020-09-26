d6ea8b9ed68807e383276ec1d95df6025af55981
Page <gigolopage@gmail.com>
2020-09-26 10:10:06 +0900

    09-26-2020 list,addLayout

--- app/src/main/java/com/divein/divelog/MainActivity.java
+++ app/src/main/java/com/divein/divelog/MainActivity.java
@@ -2,13 +2,29 @@ package com.divein.divelog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent in_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        in_add=new Intent(this,log_add.class);
+    }
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.btn_add:
+                startActivity(in_add);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/divein/divelog/log_add.java
@@ -0,0 +1,14 @@
+package com.divein.divelog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class log_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_add);
+    }
+}
